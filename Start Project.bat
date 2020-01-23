b9539c28fef53c33b13c233716509249b1d1a660
AbzDev <42191357+AbzDev@users.noreply.github.com>
2020-01-23 13:21:10 +0000

    Minor Adjustments to Login Page

--- Start Project.bat
+++ Start Project.bat
@@ -1,11 +1,7 @@
 @echo off
 cd /
 REM This points to the correct directory for a local github install of the project.
-<<<<<<< Updated upstream
-cd C:\Users\Abdullah Shah\OneDrive\Documents\GitHub\csgo-e-commerce-website\TheGameCompanyStore\
-=======
-cd C:\Users\Abdullah Shah\OneDrive\Documents\GitHub\TheGameCompanyStore\TeamX
->>>>>>> Stashed changes
+cd C:\Users\Abdullah Shah\OneDrive\Documents\GitHub\ShopWebsiteTemplate\ExampleStore
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver
