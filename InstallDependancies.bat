c4cb490815e34e57bfeb1b834806c15a8f15b767
AbzDev <42191357+AbzDev@users.noreply.github.com>
2020-02-13 16:11:07 +0000

    Update to dependencies

--- InstallDependancies.bat
+++ InstallDependancies.bat
@@ -3,3 +3,5 @@ REM It also means it will only atempt to install the required libraries for you.
 pip install Pillow --user
 pip install Django==2.2.3 --user
 pip install pylint-django --user
+pip install phonenumbers --user
+pip install django-phonenumber-field --user
