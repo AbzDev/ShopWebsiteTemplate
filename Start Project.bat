dce6cd9625213ab3132a134f59457691b31f04e6
Abdullah Shah <42191357+Abzzdev@users.noreply.github.com>
2020-09-28 11:54:45 +0100

    Update Start Project.bat

--- Start Project.bat
+++ Start Project.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /
 REM This points to the correct directory for a local github install of the project.
-cd C:\Users\Abdullah Shah\OneDrive\Documents\GitHub\ShopWebsiteTemplate\ExampleStore
+cd C:\Users\abzzd\Documents\GitHub\ShopWebsiteTemplate\ExampleStore
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver
